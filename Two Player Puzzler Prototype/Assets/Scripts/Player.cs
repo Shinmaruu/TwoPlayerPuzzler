e9cae8e337eaf1621b07811be98e8df27f4d625d
Shinmaruu <143132888+Shinmaruu@users.noreply.github.com>
2024-03-21 19:08:25 -0400

    nice commit

--- Two Player Puzzler Prototype/Assets/Scripts/Player.cs
+++ Two Player Puzzler Prototype/Assets/Scripts/Player.cs
@@ -10,47 +10,54 @@ public class Player : MonoBehaviour
     [SerializeField] GameObject player1;
     [SerializeField] GameObject player2;
 
-    private float tether = 10;
+    private Rigidbody player1RB;
+    private Rigidbody player2RB;
 
+    [SerializeField] SpringJoint rope;
 
-
-    private void Update()
+    public void Start()
     {
-        if (Vector3.Distance(player1.transform.position, player2.transform.position) < tether)
-        {
-            if (Input.GetKey(KeyCode.UpArrow))
-            {
-                player1.transform.position += Vector3.forward * speed * Time.deltaTime;
-            }
-            if (Input.GetKey(KeyCode.DownArrow))
-            {
-                player1.transform.position += Vector3.back * speed * Time.deltaTime;
-            }
-            if (Input.GetKey(KeyCode.RightArrow))
-            {
-                player1.transform.position += Vector3.right * speed * Time.deltaTime;
-            }
-            if (Input.GetKey(KeyCode.LeftArrow))
-            {
-                player1.transform.position += Vector3.left * speed * Time.deltaTime;
-            }
+        player1RB = player1.GetComponent<Rigidbody>();
+        player2RB = player2.GetComponent<Rigidbody>();
 
-            if (Input.GetKey(KeyCode.W))
-            {
-                player2.transform.position += Vector3.forward * speed * Time.deltaTime;
-            }
-            if (Input.GetKey(KeyCode.S))
-            {
-                player2.transform.position += Vector3.back * speed * Time.deltaTime;
-            }
-            if (Input.GetKey(KeyCode.D))
-            {
-                player2.transform.position += Vector3.right * speed * Time.deltaTime;
-            }
-            if (Input.GetKey(KeyCode.A))
-            {
-                player2.transform.position += Vector3.left * speed * Time.deltaTime;
-            }
+        rope.anchor = player2.transform.position;
+        rope.connectedAnchor = player1.transform.position;
+    }
+
+    public void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            player1RB.AddForce(Vector3.forward * speed, ForceMode.Force);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            player1RB.AddForce(Vector3.back * speed, ForceMode.Force);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            player1RB.AddForce(Vector3.right * speed, ForceMode.Force);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            player1RB.AddForce(Vector3.left * speed, ForceMode.Force);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            player2RB.AddForce(Vector3.forward * speed, ForceMode.Force);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            player2RB.AddForce(Vector3.back * speed, ForceMode.Force);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            player2RB.AddForce(Vector3.right * speed, ForceMode.Force);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            player2RB.AddForce(Vector3.left * speed, ForceMode.Force);
         }
     }
 
